84fad1d9189060ae58ab984f4d48ce9ec82e46af
rogersanick <nicholasrogers@MacBook-Dev-09.local>
2018-10-31 17:10:20 -0400

    All tests passing for ContractTests moving on to FlowTests

--- src/main/java/java_bootcamp/TokenContract.java
+++ src/main/java/java_bootcamp/TokenContract.java
@@ -4,7 +4,12 @@ import net.corda.core.contracts.Command;
 import net.corda.core.contracts.CommandData;
 import net.corda.core.contracts.Contract;
 import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.Party;
 import net.corda.core.transactions.LedgerTransaction;
+import sun.tools.jstat.Token;
+
+import java.security.PublicKey;
+import java.util.List;
 
 import static net.corda.core.contracts.ContractsDSL.requireThat;
 
@@ -20,7 +25,43 @@ public class TokenContract implements Contract {
 
     @Override
     public void verify(LedgerTransaction tx) throws IllegalArgumentException {
+        if (tx.getCommands().size() != 1)
+            throw new IllegalArgumentException("Transaction must have exactly one command.");
+        Command command = tx.getCommand(0);
+        List<PublicKey> requiredSigners = command.getSigners();
+        CommandData commandType = command.getValue();
+
+        if (commandType instanceof Commands.Issue) {
+
+            // Shape
+            if (tx.getInputStates().size() != 0) throw new IllegalArgumentException("Transaction should have no inputs");
+            if (tx.getOutputStates().size() != 1) throw new IllegalArgumentException("Transaction should have no inputs");
+                // Why do we use both of these types of checks?
+            if (tx.inputsOfType(TokenState.class).size() != 0) throw new IllegalArgumentException("Transaction should have no inputs");
+            if (tx.outputsOfType(TokenState.class).size() != 1) throw new IllegalArgumentException("Transaction should have no inputs");
+
+            // Contents
+            ContractState outputState = tx.getOutput(0);
+
+            if (!(outputState instanceof TokenState)) throw new IllegalArgumentException("Transaction does not output a TokenState");
+
+            TokenState tokenState = (TokenState) outputState;
+
+            if (!(tokenState.getAmount() > 0)) throw new IllegalArgumentException("Transaction outputs must be positive");
+
+            // Required Signers
+            Party owner = tokenState.getOwner();
+            PublicKey ownersKey = owner.getOwningKey();
+
+            Party issuer = tokenState.getIssuer();
+            PublicKey issuersKey = issuer.getOwningKey();
+
+            if (!(requiredSigners.contains(issuersKey))) throw new IllegalArgumentException("Transaction should be signed by the issuer");
+
+
+        } else {
+            throw new IllegalArgumentException("Transaction command should be of type issue");
+        }
 
     }
-
 }
