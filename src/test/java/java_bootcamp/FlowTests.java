1111960ceb8dffcee9e5f1d3cf10fa28ceeb8192
rogersanick <nicholasrogers@MacBook-Dev-09.local>
2018-11-02 13:40:14 -0400

    Fixing a git nightmare

--- src/test/java/java_bootcamp/FlowTests.java
+++ src/test/java/java_bootcamp/FlowTests.java
@@ -31,11 +31,6 @@ public class FlowTests {
         network.stopNodes();
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // START TOKEN ISSUE TESTS
-
     @Test
     public void transactionConstructedByFlowUsesTheCorrectNotary() throws Exception {
         TokenIssueFlow flow = new TokenIssueFlow(nodeB.getInfo().getLegalIdentities().get(0), 99);
@@ -86,7 +81,7 @@ public class FlowTests {
         assertEquals(1, signedTransaction.getTx().getCommands().size());
         Command command = signedTransaction.getTx().getCommands().get(0);
 
-        assert(command.getValue() instanceof TokenContract.Commands.Issue);
+        assert (command.getValue() instanceof TokenContract.Commands.Issue);
     }
 
     @Test
@@ -100,7 +95,7 @@ public class FlowTests {
         Command command = signedTransaction.getTx().getCommands().get(0);
 
         assertEquals(1, command.getSigners().size());
-        assert(command.getSigners().contains(nodeA.getInfo().getLegalIdentities().get(0).getOwningKey()));
+        assert (command.getSigners().contains(nodeA.getInfo().getLegalIdentities().get(0).getOwningKey()));
     }
 
     @Test
@@ -115,98 +110,4 @@ public class FlowTests {
         assertEquals(1, signedTransaction.getTx().getAttachments().size());
         assertEquals(null, signedTransaction.getTx().getTimeWindow());
     }
-
-    // START TOKEN TRANSFER TESTS
-
-=======
-=======
->>>>>>> parent of 4e739f8... All tests passing for basic TokenIssue content
-=======
->>>>>>> parent of 4e739f8... All tests passing for basic TokenIssue content
-//    @Test
-//    public void transactionConstructedByFlowUsesTheCorrectNotary() throws Exception {
-//        TokenIssueFlow flow = new TokenIssueFlow(nodeB.getInfo().getLegalIdentities().get(0), 99);
-//        CordaFuture<SignedTransaction> future = nodeA.startFlow(flow);
-//        network.runNetwork();
-//        SignedTransaction signedTransaction = future.get();
-//
-//        assertEquals(1, signedTransaction.getTx().getOutputStates().size());
-//        TransactionState output = signedTransaction.getTx().getOutputs().get(0);
-//
-//        assertEquals(network.getNotaryNodes().get(0).getInfo().getLegalIdentities().get(0), output.getNotary());
-//    }
-
-//    @Test
-//    public void transactionConstructedByFlowHasOneTokenStateOutputWithTheCorrectAmountAndOwner() throws Exception {
-//        TokenIssueFlow flow = new TokenIssueFlow(nodeB.getInfo().getLegalIdentities().get(0), 99);
-//        CordaFuture<SignedTransaction> future = nodeA.startFlow(flow);
-//        network.runNetwork();
-//        SignedTransaction signedTransaction = future.get();
-//
-//        assertEquals(1, signedTransaction.getTx().getOutputStates().size());
-//        TokenState output = signedTransaction.getTx().outputsOfType(TokenState.class).get(0);
-//
-//        assertEquals(nodeB.getInfo().getLegalIdentities().get(0), output.getOwner());
-//        assertEquals(99, output.getAmount());
-//    }
-
-//    @Test
-//    public void transactionConstructedByFlowHasOneOutputUsingTheCorrectContract() throws Exception {
-//        TokenIssueFlow flow = new TokenIssueFlow(nodeB.getInfo().getLegalIdentities().get(0), 99);
-//        CordaFuture<SignedTransaction> future = nodeA.startFlow(flow);
-//        network.runNetwork();
-//        SignedTransaction signedTransaction = future.get();
-//
-//        assertEquals(1, signedTransaction.getTx().getOutputStates().size());
-//        TransactionState output = signedTransaction.getTx().getOutputs().get(0);
-//
-//        assertEquals("java_bootcamp.TokenContract", output.getContract());
-//    }
-
-//    @Test
-//    public void transactionConstructedByFlowHasOneIssueCommand() throws Exception {
-//        TokenIssueFlow flow = new TokenIssueFlow(nodeB.getInfo().getLegalIdentities().get(0), 99);
-//        CordaFuture<SignedTransaction> future = nodeA.startFlow(flow);
-//        network.runNetwork();
-//        SignedTransaction signedTransaction = future.get();
-//
-//        assertEquals(1, signedTransaction.getTx().getCommands().size());
-//        Command command = signedTransaction.getTx().getCommands().get(0);
-//
-//        assert(command.getValue() instanceof TokenContract.Commands.Issue);
-//    }
-
-//    @Test
-//    public void transactionConstructedByFlowHasOneCommandWithTheIssueAsASigner() throws Exception {
-//        TokenIssueFlow flow = new TokenIssueFlow(nodeB.getInfo().getLegalIdentities().get(0), 99);
-//        CordaFuture<SignedTransaction> future = nodeA.startFlow(flow);
-//        network.runNetwork();
-//        SignedTransaction signedTransaction = future.get();
-//
-//        assertEquals(1, signedTransaction.getTx().getCommands().size());
-//        Command command = signedTransaction.getTx().getCommands().get(0);
-//
-//        assertEquals(1, command.getSigners().size());
-//        assert(command.getSigners().contains(nodeA.getInfo().getLegalIdentities().get(0).getOwningKey()));
-//    }
-
-//    @Test
-//    public void transactionConstructedByFlowHasNoInputsAttachmentsOrTimeWindows() throws Exception {
-//        TokenIssueFlow flow = new TokenIssueFlow(nodeB.getInfo().getLegalIdentities().get(0), 99);
-//        CordaFuture<SignedTransaction> future = nodeA.startFlow(flow);
-//        network.runNetwork();
-//        SignedTransaction signedTransaction = future.get();
-//
-//        assertEquals(0, signedTransaction.getTx().getInputs().size());
-//        // The single attachment is the contract attachment.
-//        assertEquals(1, signedTransaction.getTx().getAttachments().size());
-//        assertEquals(null, signedTransaction.getTx().getTimeWindow());
-//    }
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 4e739f8... All tests passing for basic TokenIssue content
-=======
->>>>>>> parent of 4e739f8... All tests passing for basic TokenIssue content
-=======
->>>>>>> parent of 4e739f8... All tests passing for basic TokenIssue content
 }
